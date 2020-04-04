e460eefd5e91c37e3dd89ce74e2e0f91b1c902a8
Rasha ali <rashaali84@gmail.com>
2020-04-04 17:02:26 +0200

    fix idex.html with correct links

--- app/app.js
+++ app/app.js
@@ -2,17 +2,18 @@
 
 /* Play-Pause user-story 1 */
 function togglePlay() {
-    debugger;
+
     const method = video.paused ? 'play' : 'pause';
     video[method]();
 }
 function updateButton() {
-    debugger;
+
     const icon = this.paused ? '►' : '❚ ❚';
     console.log(icon);
     toggle.textContent = icon;
 }
 //Skip user-story 2 
 function skip() {
+    debugger;
     video.currentTime += parseFloat(this.dataset.skip);
 }
